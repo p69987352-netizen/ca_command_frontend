422122bc530e0bcef4877551db4ed95c6affcbe6
bhanu250506 <bhanupratap2556@gmail.com>
2026-06-30 09:40:29 +0530

    Fix build script for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/ca_command_frontend/',
   plugins: [react()],
   resolve: {
     alias: {
